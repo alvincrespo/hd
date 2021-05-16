cca3d14502efbebe8eb4ed52a1ab32a212c126e7
Alvin Crespo <alvin.crespo@gmail.com>
2021-05-16 14:36:58 -0400

    add healthcheckFailed hook

---
+++ src/utils/healthcheckFailed.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const { execSync } = require("child_process");
+
+const rollback = (app_name, appdir) => {
+  const command = `heroku rollback --app ${app_name}`;
+  const options = appdir ? { cwd: appdir } : null;
+
+  execSync(command, options);
+
+  const message = "Health Check Failed. Error deploying Server. Deployment has been rolled back. Please check your logs on Heroku to try and diagnose the problem";
+  core.setFailed(message);
+};
+
+export const healthcheckFailed = (config) => {
+  const { rollbackonhealthcheckfailed } = config;
+  if (rollbackonhealthcheckfailed) {
+    const { app_name, appdir } = config
+    rollback(app_name, appdir);
+  } else {
+    const message = "Health Check Failed. Error deploying Server. Please check your logs on Heroku to try and diagnose the problem";
+    core.setFailed(message);
+  }
+};
