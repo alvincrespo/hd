16a6031ac3e792afdb91dc5876642760e1fbe329
Alvin Crespo <alvin.crespo@gmail.com>
2021-05-16 14:08:35 -0400

    support adding config

---
+++ src/utils/addConfig.js
@@ -0,0 +1,35 @@
+const loadEnvironmentVariables = () => {
+  const envVars = [];
+
+  for (let key in process.env) {
+    if (key.startsWith("HD_")) {
+      envVars.push(key.substring(3) + "='" + process.env[key] + "'");
+    }
+  }
+
+  return envVars;
+}
+
+const loadEnvironmentVariablesFromFile = (env_file) => {
+  const envVars = [];
+
+  if (env_file) {
+    const env = fs.readFileSync(path.join(appdir, env_file), "utf8");
+    const variables = require("dotenv").parse(env);
+    for (let key in variables) {
+      envVars.push(key + "=" + variables[key]);
+    }
+  }
+
+  return envVars;
+}
+
+export const addConfig = ({ app_name, env_file, appdir }) => {
+  const envVars = loadEnvironmentVariables();
+  const envVarsFromFile = loadEnvironmentVariablesFromFile(env_file);
+  const configVars = [...envVars, ...envVarsFromFile];
+
+  if (configVars.length !== 0) {
+    execSync(`heroku config:set --app=${app_name} ${configVars.join(" ")}`);
+  }
+};
